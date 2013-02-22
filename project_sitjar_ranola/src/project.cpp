6505c4708967522581e1d6c9c9350ec68a7ac459
jonaranola <jonaxranola@yahoo.com>
2013-02-23 00:49:27 +0800

    edit

--- project_sitjar_ranola/src/project.cpp
+++ project_sitjar_ranola/src/project.cpp
@@ -278,112 +278,113 @@ bool project::Prime_check(long num){
      }
      return Prime_check;
 }
-// Problem 11
-void project::largest_product_grid(){
 
-int num_container[20][20] =
+void project::largest_product_grid()// Problem 11
 {
-{ 8,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91, 8},
-{49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,00},
-{81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65},
-{52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91},
-{22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80},
-{24,47,32,60,99,03,45,02,44,75,33,53,78,36,84,20,35,17,12,50},
-{32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70},
-{67,26,20,68,02,62,12,20,95,63,94,39,63, 8,40,91,66,49,94,21},
-{24,55,58,05,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72},
-{21,36,23, 9,75,00,76,44,20,45,35,14,00,61,33,97,34,31,33,95},
-{78,17,53,28,22,75,31,67,15,94,03,80,04,62,16,14, 9,53,56,92},
-{16,39,05,42,96,35,31,47,55,58,88,24,00,17,54,24,36,29,85,57},
-{86,56,00,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58},
-{19,80,81,68,05,94,47,69,28,73,92,13,86,52,17,77,04,89,55,40},
-{04,52, 8,83,97,35,99,16,07,97,57,32,16,26,26,79,33,27,98,66},
-{88,36,68,87,57,62,20,72,03,46,33,67,46,55,12,32,63,93,53,69},
-{04,42,16,73,38,25,39,11,24,94,72,18, 8,46,29,32,40,62,76,36},
-{20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,04,36,16},
-{20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54},
-{01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48},
-                                                            };
 
-int start = 0;
-int end = 3;
-int product = 1;
+    int num_container[20][20] =
+    {
+    { 8,02,22,97,38,15,00,40,00,75,04,05,07,78,52,12,50,77,91, 8},
+    {49,49,99,40,17,81,18,57,60,87,17,40,98,43,69,48,04,56,62,00},
+    {81,49,31,73,55,79,14,29,93,71,40,67,53,88,30,03,49,13,36,65},
+    {52,70,95,23,04,60,11,42,69,24,68,56,01,32,56,71,37,02,36,91},
+    {22,31,16,71,51,67,63,89,41,92,36,54,22,40,40,28,66,33,13,80},
+    {24,47,32,60,99,03,45,02,44,75,33,53,78,36,84,20,35,17,12,50},
+    {32,98,81,28,64,23,67,10,26,38,40,67,59,54,70,66,18,38,64,70},
+    {67,26,20,68,02,62,12,20,95,63,94,39,63, 8,40,91,66,49,94,21},
+    {24,55,58,05,66,73,99,26,97,17,78,78,96,83,14,88,34,89,63,72},
+    {21,36,23, 9,75,00,76,44,20,45,35,14,00,61,33,97,34,31,33,95},
+    {78,17,53,28,22,75,31,67,15,94,03,80,04,62,16,14, 9,53,56,92},
+    {16,39,05,42,96,35,31,47,55,58,88,24,00,17,54,24,36,29,85,57},
+    {86,56,00,48,35,71,89,07,05,44,44,37,44,60,21,58,51,54,17,58},
+    {19,80,81,68,05,94,47,69,28,73,92,13,86,52,17,77,04,89,55,40},
+    {04,52, 8,83,97,35,99,16,07,97,57,32,16,26,26,79,33,27,98,66},
+    {88,36,68,87,57,62,20,72,03,46,33,67,46,55,12,32,63,93,53,69},
+    {04,42,16,73,38,25,39,11,24,94,72,18, 8,46,29,32,40,62,76,36},
+    {20,69,36,41,72,30,23,88,34,62,99,69,82,67,59,85,74,04,36,16},
+    {20,73,35,29,78,31,90,01,74,31,49,71,48,86,81,16,23,57,05,54},
+    {01,70,54,71,83,51,54,69,16,92,33,48,61,43,52,01,89,19,67,48},
+                                                                };
 
-vector<long int>final_results;
+    int start = 0;
+    int end = 3;
+    int product = 1;
 
-    for(int i=0; i<20; i++){
-        for(int j=start; j<=end; j++){
-            product=product*num_container[i][j];
-            if(j==end){
-            final_results.push_back(product);
-            product=1;
-            start++;
-            end++;
+    vector<long int>final_results;
+
+        for(int i=0; i<20; i++){
+            for(int j=start; j<=end; j++){
+                product=product*num_container[i][j];
+                if(j==end){
+                final_results.push_back(product);
+                product=1;
+                start++;
+                end++;
+                }
+                if(j==19)break;
             }
-            if(j==19)break;
-        }
-        start=0;
-        end=3;
-        for(int k=start; k<=end; k++){
-            product=product*num_container[k][i];
-            if(k==end){
-            final_results.push_back(product);
-            product=1;
-            start++;
-            end++;
-            }
-            if(k==19){
             start=0;
             end=3;
-            break;
+            for(int k=start; k<=end; k++){
+                product=product*num_container[k][i];
+                if(k==end){
+                final_results.push_back(product);
+                product=1;
+                start++;
+                end++;
+                }
+                if(k==19){
+                start=0;
+                end=3;
+                break;
+                }
             }
         }
-    }
 
-int current_row = 0;
+    int current_row = 0;
 
-    for(int i=0; i<=16; i++)
-    {
-        for(int j=0; j<=16; j++)
+        for(int i=0; i<=16; i++)
         {
-            current_row = i;
-            for(int k=start; k<=end; k++)
+            for(int j=0; j<=16; j++)
             {
-                product = product * num_container[current_row][k];
-                current_row++;
-                if (k==end)
-                    final_results.push_back(product);
+                current_row = i;
+                for(int k=start; k<=end; k++)
+                {
+                    product = product * num_container[current_row][k];
+                    current_row++;
+                    if (k==end)
+                        final_results.push_back(product);
+                }
+                product = 1;
+                start++;
+                end++;
             }
-            product = 1;
-            start++;
-            end++;
-        }
-        start = 0;
-        end = 3;
+            start = 0;
+            end = 3;
 
-        for(int j=0; j<=16; j++)
-        {
-            current_row = i+3;
-            for(int k=start; k<=end; k++)
+            for(int j=0; j<=16; j++)
             {
-                product = product * num_container[current_row][k];
-                current_row--;
-                if (k==end)
-                    final_results.push_back(product);
+                current_row = i+3;
+                for(int k=start; k<=end; k++)
+                {
+                    product = product * num_container[current_row][k];
+                    current_row--;
+                    if (k==end)
+                        final_results.push_back(product);
+                }
+                product = 1;
+                start++;
+                end++;
             }
-            product = 1;
-            start++;
-            end++;
+            start = 0;
+            end = 3;
         }
-        start = 0;
-        end = 3;
-    }
-    int max_product=0;
-    for(unsigned p=0; p<final_results.size(); p++){
-        if(final_results[p]>max_product)
-            max_product=final_results[p];
-    }
-    cout << max_product << endl;
+        int max_product=0;
+        for(unsigned p=0; p<final_results.size(); p++){
+            if(final_results[p]>max_product)
+                max_product=final_results[p];
+        }
+        cout << "The greatest product of four adjacent numbers: " << max_product << endl;
 }
 
 void project::divisible_triangular_number() // Problem 12
@@ -544,7 +545,7 @@ void project::largest_sum() // Problem 13
 
         j++;
       }
-      cout <<  string.c_str() << endl;
+      cout <<  "The sum of the first ten digit is: " << string.c_str() << endl;
 }
 
 void project::longest_collatz_sequence() // Problem 14
@@ -676,7 +677,7 @@ void project::number_letter_counts() // Problem 17
       }
 
       count += onek.length() - 1;
-      cout << count << endl;
+      cout << "The number of letters: " << count << endl;
 }
 
 void project::max_path_sum_one() // Problem 18
@@ -749,7 +750,7 @@ void project::max_path_sum_one() // Problem 18
     for (int j = 0; j < (i - 1); ++j)
       data[i-2][j] += std::max(data[i-1][j], data[i-1][j+1]);
 
-  cout << data[0][0] << endl;
+  cout << "The maximum total from top to bottom of the triangle: " << data[0][0] << endl;
 
 }
 
@@ -789,7 +790,7 @@ void project::counting_sundays() // Problem 19
         }
     }
 
-    cout << count << endl;
+    cout << "The number of sundays: " << count << endl;
 
 
 
